cfff7f9e2487ed978df7d772cd482776b42c4165
Natnael1234 <natimeng1269@gmail.com>
2025-10-01 00:25:33 +0300

    second commit

--- components/header.tsx
+++ components/header.tsx
@@ -24,7 +24,6 @@ export function Header() {
   { label: "Gallery", href: "/gallery" },
   { label: "Contact", href: "/#contact" },
 ]
-
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
